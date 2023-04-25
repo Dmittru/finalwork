5faebab4d5be62905145f52bc0ddcba0b7721e23
CERF <limbooo689@gmail.com>
2023-04-25 16:28:05 +0300

    beta v0.9.1

--- src/Components/pages/LessonPage/LessonContent.jsx
+++ src/Components/pages/LessonPage/LessonContent.jsx
@@ -1,11 +1,143 @@
 import React from 'react';
 
-const LessonContent = () => {
+const LessonBeforeAll = () => {
     return (
         <div>
-
+            <div className='headlesson'>
+                <h1>
+                    Введение: начало
+                </h1>
+                <p className='headshadowtext'>
+                    Прежде, чем начинать, прочтите это наставление...
+                </p>
+            </div>
+            <p style={{fontSize:'1.3em'}}>
+                ffi fifi fifif ifi
+            </p>
+        </div>
+    );
+}
+const LessonRoadmaps = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonLangChoice = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonOnlineSch = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonTutors = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonSelfLearn = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonSkillbox = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonSkillfactory = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonYaPractice = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonGeekbrains = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonYT = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonForums = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonOnlineBooks = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonNeurolinks = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonBooks = () => {
+    return (
+        <div>
+            <p>Before123</p>
+        </div>
+    );
+};
+const LessonUsefulLinks = () => {
+    return (
+        <div>
+            <p>Before</p>
+        </div>
+    );
+}
+const LessonLost = () => {
+    return (
+        <div>
+            <div className='headlesson'>
+                <h1>
+                    Данного урока не существует
+                </h1>
+                <p className='headshadowtext'>
+                    Вы перешли на несуществующий, либо удалённый урок
+                </p>
+            </div>
+            <p style={{fontSize:'1.3em'}}>
+                Попробуйте вернуться и выбрать другой урок
+            </p>
         </div>
     );
 };
 
-export {LessonContent};
+export {LessonBeforeAll, LessonRoadmaps, LessonLangChoice, LessonBooks, LessonForums, LessonGeekbrains, LessonLost, LessonNeurolinks, LessonOnlineBooks, LessonOnlineSch, LessonSelfLearn, LessonSkillbox, LessonSkillfactory, LessonTutors, LessonUsefulLinks, LessonYaPractice, LessonYT};
--- src/Components/pages/LessonPage/LessonPage.jsx
+++ src/Components/pages/LessonPage/LessonPage.jsx
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../styles/LessonPage.css'
+import {Container, OverlayTrigger, Tooltip} from "react-bootstrap";
+import {useParams} from "react-router-dom";
+import {
+    LessonBeforeAll, LessonBooks, LessonForums, LessonGeekbrains,
+    LessonLangChoice, LessonLost, LessonNeurolinks, LessonOnlineBooks,
+    LessonOnlineSch,
+    LessonRoadmaps,
+    LessonSelfLearn, LessonSkillbox, LessonSkillfactory,
+    LessonTutors, LessonUsefulLinks, LessonYaPractice, LessonYT
+} from "./LessonContent";
+import config from "../../../config.json";
 
 const LessonPage = () => {
+    const { type } = useParams();
+    const [overlayLable, setOverlayLable] = useState('В начало');
+    const [nextPath, setNextPath] = useState('/book/main');
+
+
+
+
+    useEffect(() => {
+        const getNextTitleByTitle = (title) => {
+            for (const section in config) {
+                const themes = config[section].themes;
+                for (let i = 0; i < themes.length; i++) {
+                    if ((themes[i].path).includes(type)) {
+                        setOverlayLable(themes[i].nextTitle);
+                        setNextPath(themes[i].next);
+                    }
+                }
+            }
+            return 'В начало';
+        };
+        getNextTitleByTitle(type);
+    }, [type]);
+
     return (
-        <div>
-            
+        <div style={{overflow:'hidden', position:'relative'}}>
+            <OverlayTrigger
+                placement="left"
+                overlay={
+                    <Tooltip id="tooltip-verified">{overlayLable}</Tooltip>
+                }
+            >
+                <div
+                    className='nextskiper'
+                    style={{height: '100%', position: 'absolute', right: 0, top: 0, width: '100px'}}
+                    onClick={()=>{
+                        window.location.href = nextPath;
+                    }}
+                >
+                    ->
+                </div>
+            </OverlayTrigger>
+            <Container style={{paddingTop:'50px', paddingBottom:'50px', minHeight:'85vh', position: 'relative'}}>
+
+                {
+                    type === 'before-all' ? <LessonBeforeAll/> :
+                        type === 'roadmaps' ? <LessonRoadmaps/> :
+                            type === 'web-language' ? <LessonLangChoice/> :
+                                type === 'online-schools' ? <LessonOnlineSch/> :
+                                    type === 'tutors' ? <LessonTutors/> :
+                                        type === 'self-learning' ? <LessonSelfLearn/> :
+                                            type === 'skillbox' ? <LessonSkillbox/> :
+                                                type === 'skillfactory' ? <LessonSkillfactory/> :
+                                                    type === 'yandexprancticum' ? <LessonYaPractice/> :
+                                                        type === 'geekbrains' ? <LessonGeekbrains/> :
+                                                            type === 'youtube' ? <LessonYT/> :
+                                                                type === 'forums' ? <LessonForums/> :
+                                                                    type === 'online-books' ? <LessonOnlineBooks/> :
+                                                                        type === 'neurolinks' ? <LessonNeurolinks/> :
+                                                                            type === 'books' ? <LessonBooks/> :
+                                                                                type === 'useful-links' ? <LessonUsefulLinks/> :
+                                                                                    <LessonLost/>
+                }
+            </Container>
         </div>
     );
 };
 
 export default LessonPage;
+
